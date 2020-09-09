dcef918cc29bd8434f626dc8241b3efa65b3e34b
¨Certificacion¨ <espanata@espe.edu.ec>
2020-09-08 22:12:15 -0500

    arreglado

--- src/main/java/com/grupal/inmobilaria/dao/IUsuario.java
+++ src/main/java/com/grupal/inmobilaria/dao/IUsuario.java
@@ -13,7 +13,10 @@ public interface IUsuario  extends CrudRepository<Usuario, Integer>{
 	public Usuario findByNombre(String nombre);
 	
 	//Clasificacion de usuarios por roles 
-	@Query("Select U from Usuario U  left join Rol R on U.idusuario = R.idrol where R.nombre = :id")
+	//Select * from usuarios U left join roles R on U.pk_usuario = R.fk_usuario where R.nombre = "ROLE_ADMIN"
+	//@Query(value = "Select U from Usuario U  left join Rol R on U.idusuario = R.idrol where R.nombre = :id")
+	
+	@Query(value = "Select * from usuarios U left join roles R on U.pk_usuario = R.fk_usuario where R.nombre = :id",  nativeQuery = true)
 	public List<Usuario> findByRoles(String id);
 	//Rol roles = new Rol();
 	
